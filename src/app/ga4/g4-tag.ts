1e88eacc63d5ccd9523844ea1cc0c8687dc8c8a9
tozzen0121 <tozzen0121@gmail.com>
2025-05-27 09:13:33 -0400

    added-id

--- src/app/ga4/g4-tag.ts
+++ src/app/ga4/g4-tag.ts
@@ -10,7 +10,7 @@ declare global {
     }
 }
 
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || ''
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || 'G-1RBGXM1DY8'
 
 // Initialize GA4
 export const initGA = (): Promise<void> => {
